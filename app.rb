4089f4f264fbff8eb3cefe2a88a8f569bd376964
kiburei <akiburei@gmail.com>
2017-07-24 11:59:52 +0300

    add root url, new pun root and success/error redirect

--- app.rb
+++ app.rb
@@ -1,3 +1,21 @@
 require 'bundler/setup'
 Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get '/' do
+  @puns = Pun.all
+  erb(:index)
+end
+
+post '/puns' do
+  @pun = Pun.new(pun: params.fetch('pun'), upvote: nil, downvote: nil, author_id: nil)
+  if @pun.save
+    redirect '/'
+  else
+    redirect '/error'
+  end
+end
+
+get '/error' do
+  erb(:error)
+end
