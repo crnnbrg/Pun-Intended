1d3617dc5f962f7bcc0ca2f1239440e6ccb7eb98
kiburei <akiburei@gmail.com>
2017-07-24 11:50:48 +0300

    create table puns

---
+++ db/migrate/20170724084333_create_puns.rb
@@ -0,0 +1,10 @@
+class CreatePuns < ActiveRecord::Migration[5.1]
+  def change
+    create_table(:puns) do |t|
+      t.column(:pun, :string)
+      t.column(:upvote, :integer)
+      t.column(:downvote, :integer)
+      t.column(:author_id, :integer)
+    end
+  end
+end
