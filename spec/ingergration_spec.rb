c8a7859966b08e57f8b9822900e313b65073f5ed
newtonkiragu <karanunewton4@gmail.com>
2017-07-24 14:31:50 +0300

    made the first test

--- spec/ingergration_spec.rb
+++ spec/ingergration_spec.rb
@@ -1,3 +1,10 @@
 require('spec_helper')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+
+describe('showing the landing page', {:type => :feature}) do
+  it"shows the landing page on opening the website" do
+    visit('/')
+    expect(page).to have_content('index')
+  end
+end
