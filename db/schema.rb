f8f7d88468bd3fd24e77450172d825b5ae8771a2
kiburei <akiburei@gmail.com>
2017-07-26 15:38:23 +0300

    Add user autrhenticaton with sessions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170724154148) do
+ActiveRecord::Schema.define(version: 20170726094237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 20170724154148) do
     t.integer "downvote", default: 0
     t.string "author_id"
     t.integer "blonde", default: 0
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password"
   end
 
 end
--- lib/user.rb
+++ lib/user.rb
@@ -1,19 +1,3 @@
-require 'rubygems'
-require 'data_mapper'
-require 'dm-sqlite-adapter'
-require 'bcrypt'
-
-DataMapper.setup(:default, "sqlite://#{Dir.pwd}/db.sqlite")
-
-class User
-  include DataMapper::Resource
-  include BCrypt
-
-  property :id, Serial, :key => true
-  property :username, String, :length => 3..5
-  property :password, BCryptHash
+class User < ActiveRecord::Base
 
 end
-
-DataMapper.finalize
-DataMapper.auto_upgrade!
